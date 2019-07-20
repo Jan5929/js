39384631af4fcd2479181ecac6fcff2069d824ff
Jaylene <l592965@163.com>
2019-07-20 17:57:57 +0800

    小程序首页

--- demo/app.js
+++ demo/app.js
@@ -1,45 +1,15 @@
 //app.js demo
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    var windowHeight = null
-    // 屏幕适配
-    wx.getSystemInfo({
-      success: function (res) {
-        console.log(res)
-      }
-    })
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+  onLaunch (options) { // 生命周期回调——监听小程序初始化。
+    console.log(options, 1)
   },
-  globalData: {
-    userInfo: null
-  }
+  onShow(options) {  // 生命周期回调——监听小程序启动或切前台
+    console.log(options, 2)
+  },
+  onHide() { // 生命周期回调——监听小程序切后台
+    
+  },
+  onError(msg) { // 错误监听函数
+    console.log(msg)
+  },
 })
